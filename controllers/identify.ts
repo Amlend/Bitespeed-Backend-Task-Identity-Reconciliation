91d89b5fa2d2b0ae603513e1c762cefe14731510
Amlend <amlendsingh@gmail.com>
2024-04-24 14:10:00 +0530

    added-contact-handling-endpoint

--- controllers/identify.ts
+++ controllers/identify.ts
@@ -21,3 +21,38 @@ export const handleContacts = async (req: Request, res: Response) => {
     contact: response,
   });
 };
+
+async function getPrimaryIds(email: string, phoneNumber: string) {
+  let id1: number | null = null;
+  let id2: number | null = null;
+
+  if (email) {
+    const contact1 = await Contact.findOne({
+      where: { email: email },
+    });
+
+    id1 = getPrimaryIdFromContact(contact1);
+  }
+
+  if (phoneNumber) {
+    const contact2 = await Contact.findOne({
+      where: { phoneNumber: phoneNumber },
+    });
+
+    if (id1) {
+      id2 = getPrimaryIdFromContact(contact2);
+    } else {
+      id1 = getPrimaryIdFromContact(contact2);
+    }
+  }
+
+  if (id1 && id2) {
+    if (id1 === id2) {
+      return { primaryId: id1, secondaryId: null };
+    } else {
+      return { primaryId: id1, secondaryId: id2 };
+    }
+  } else {
+    return { primaryId: id1, secondaryId: null };
+  }
+}

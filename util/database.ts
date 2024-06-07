4f37ac2bf2458304884216ab52c8ca9e05daba5a
Amlend Singh Jadaun <40403511+Amlend@users.noreply.github.com>
2024-06-07 22:27:16 +0530

    Update database.ts

--- util/database.ts
+++ util/database.ts
@@ -6,7 +6,7 @@ const databaseConfig = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: Number(process.env.DB_PORT || 3306),
 };
 
 const sequelize = new Sequelize(databaseConfig);
